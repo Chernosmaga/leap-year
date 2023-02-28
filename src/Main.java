1706f8e082b1688559a2e7618fb8e097bd1f9eda
Chernosmaga <sergeychernosmaga1997@yandex.ru>
2023-02-28 20:39:19 +0300

    Done

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println("12.09." + year);
+        } else {
+            System.out.println("13.09." + year);
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        boolean sum = false;
+        if ((year %= 100) == 0 && (year %= 400) == 0) {
+            sum = true;
+        } else if ((year %= 4) == 0 && (year %= 100) != 0) {
+            sum = false;
+        }
+        return sum;
+    }
+}
